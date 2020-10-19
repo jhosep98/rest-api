22734ba390780449df3b547e41d5e1e3c17d071d
jhosep98 <jhosep149@hotmail.com>
2020-10-19 17:48:57 -0300

    Add method delete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import "@babel/polyfill";
 import app from "./server";
-import { connect } from "./database";
 
 const main = async () => {
   await app.listen(app.get("port"));
-  await connect();
   console.log("Server on Port:", app.get("port"));
 };
 
--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
-import express from "express";
+import express, { json } from "express";
+// Routes
+import IndexRoutes from "./routes/index.routes";
+import TaskRoutes from "./routes/tasks.routes";
+
 const app = express();
 
 // settings
 app.set("port", process.env.PORT || 3000);
 
-// routes
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// Middlewares
+app.use(json());
+
+//  routes
+app.use(IndexRoutes);
+app.use("/tasks", TaskRoutes);
 
 export default app;
